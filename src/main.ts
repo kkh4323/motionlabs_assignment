5c7203c82086bca47255cc9525ada993b2af8afe
Kangho <kkh4323@naver.com>
2025-03-30 20:07:47 +0900

    feat: set up Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,11 +10,23 @@ async function bootstrap() {
   const configService: ConfigService = app.get(ConfigService);
   const port = configService.get<number>('BACKEND_PORT') || 3000;
 
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Motionlabs OPEN API')
+    .setDescription('Motionlabs OPEN API')
+    .setVersion('1.0')
+    .addTag('Motionlabs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api-docs', app, document);
 
   await app.listen(port);
 }
